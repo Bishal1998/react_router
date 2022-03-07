61ea9e059646a0fbf90961045adf601e032fbc67
“Bishal1998@gmail.com” <Bishalpaudel1998@gmail.com>
2022-03-07 12:15:13 +0545

    Link

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,12 @@
-
+import { Link } from "react-router-dom"
 
 const Navbar = () => {
   return (
     <>
         <ul>
-            <li><a href="/">Home</a></li>
-            <li><a href="/about">About</a></li>
-            <li><a href="/contact">Contact</a></li>
+            <li><Link to="/">Home</ Link></li>
+            <li><Link to="/about">About</ Link></li>
+            <li><Link to="/contact">Contact</ Link></li>
         </ul>
     </>
   )
