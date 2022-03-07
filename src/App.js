29ab5dc18bd879f5cdf86403377ab7abb4153ffe
“Bishal1998@gmail.com” <Bishalpaudel1998@gmail.com>
2022-03-07 13:17:34 +0545

    conditional route

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 import Home from './components/pages/Home';
 import About from './components/pages/About';
 import Contact from './components/pages/Contact';
 import Navbar from './components/Navbar';
 import Error from './components/Error';
 import Post from './components/pages/Post';
+import Dashboard from './components/pages/Dashboard';
+import Login from './components/pages/Login';
 
 function App() {
+
+  let isLogged = false;
+
   return (
     <>
     <BrowserRouter>
@@ -17,6 +22,8 @@ function App() {
         <Route path='/contact' element = { <Contact />} />
         <Route path='/post/:category' element = { <Post />} />
         <Route path='/post/:category/:id' element = { <Post />} />
+        <Route path='/dashboard' element = { isLogged ? <Dashboard /> : <Navigate to = '/login' replace />} />
+        <Route path='/login' element = { <Login />} />
         <Route path = '*' element = { <Error />} />
       </Routes>
     </BrowserRouter>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -13,7 +13,9 @@ const Navbar = () => {
             <li><NavLink to="/about" style={style}>About</ NavLink></li>
             <li><NavLink to="/contact" style={style}>Contact</ NavLink></li>
             <li><NavLink to="/post/mobile" style={style}>Post</ NavLink></li>
-            <li><NavLink to="/post/mobile/1" style={style}>Post</ NavLink></li>
+            <li><NavLink to="/post/mobile/1" style={style}>Post with ID</ NavLink></li>
+            <li><NavLink to="/dashboard" style={style}>Dashboard</ NavLink></li>
+            <li><NavLink to="/login" style={style}>Login</ NavLink></li>
         </ul>
     </>
   )
