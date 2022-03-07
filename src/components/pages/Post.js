0cd829b239f18d58b7232958abf46d27a57fd31d
“Bishal1998@gmail.com” <Bishalpaudel1998@gmail.com>
2022-03-07 13:03:39 +0545

    useSearchParams

--- src/components/pages/Post.js
+++ src/components/pages/Post.js
@@ -1,8 +1,11 @@
-import { useParams } from "react-router-dom";
+import { useParams, useSearchParams } from "react-router-dom";
 
 const Post = () => {
 
     let {category, id} = useParams();
+    let [searchParams, setSearchParams] = useSearchParams();
+    console.log(searchParams)
+    console.log(searchParams.get('price'));
   return (
     <>
         <h1>Post - {category}</h1>
