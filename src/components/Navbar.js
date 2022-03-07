82624f82299a06297608ee4de6c1a24950c5baa4
“Bishal1998@gmail.com” <Bishalpaudel1998@gmail.com>
2022-03-07 12:57:24 +0545

    useParam

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -12,7 +12,8 @@ const Navbar = () => {
             <li><NavLink to="/" style={style}>Home</ NavLink></li>
             <li><NavLink to="/about" style={style}>About</ NavLink></li>
             <li><NavLink to="/contact" style={style}>Contact</ NavLink></li>
-            <li><NavLink to="/post" style={style}>Post</ NavLink></li>
+            <li><NavLink to="/post/mobile" style={style}>Post</ NavLink></li>
+            <li><NavLink to="/post/mobile/1" style={style}>Post</ NavLink></li>
         </ul>
     </>
   )
--- src/components/pages/Post.js
+++ src/components/pages/Post.js
@@ -1,9 +1,12 @@
-import React from 'react'
+import { useParams } from "react-router-dom";
 
 const Post = () => {
+
+    let {category, id} = useParams();
   return (
     <>
-        <h1>Post</h1>
+        <h1>Post - {category}</h1>
+        <h1>Post Id - {id}</h1>
     </>
   )
 }
